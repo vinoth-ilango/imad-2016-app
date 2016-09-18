f6e1831872e115cbc51688944c0aaa3fe919d3e0
vinoth-ilango <vinoth.ilango@gmail.com>
2016-09-18 15:51:13 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res) {
-  res.send('Article one requested, and will be server shortly...')
+  res.send('Article one requested, and will be server shortly...');
 });
 
 app.get('/article-two', function(req, res) {
-  res.send('Article two requested, and will be server shortly...')
+  res.send('Article two requested, and will be server shortly...');
 });
 
 app.get('/article-three', function(req, res) {
-  res.send('Article three requested, and will be server shortly...')
+  res.send('Article three requested, and will be server shortly...');
 });
 
 app.get('/ui/style.css', function (req, res) {
